f56b9dffc0b955743cab348f431d76142c9385ae
Bbr011 <kahdraouih7@gmail.com>
2024-10-06 14:30:43 -0700

    pascal's triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,38 +1,22 @@
 #!/usr/bin/python3
-'''Module to find Pascal's Triangle'''
+"""
+create pascal's triangle representing number n
+"""
 
 
 def pascal_triangle(n):
-    '''
-    Function to find Pascal's Triangle
-        Parameters:
-            n : number of row's of Pascal's triangle
-        Returns:
-            pascal_triangle : Binary string of the sum of a and b
-    '''
-    pascal_triangle = list()
-
+    """
+    pascal's triangle
+    """
+    k = []
     if n <= 0:
-        return pascal_triangle
-
-    # Add first 1.
-    if n > 0:
-        pascal_triangle.append([1])
-
-    # Add second line.
-    if n > 1:
-        pascal_triangle.append([1, 1])
-
-    for x in range(3, n+1):
-        pascal_triangle.append([0] * x)
-
-        # Set first and last 1
-        pascal_triangle[x-1][0] = 1
-        pascal_triangle[x-1][x-1] = 1
-
-        # Calculate middle numbers
-        for y in range(1, x-1):
-            pascal_triangle[x-1][y] = \
-                pascal_triangle[x-2][y-1] + pascal_triangle[x-2][y]
-
-    return pascal_triangle
+        return k
+    k = [[1]]
+    for i in range(1, n):
+        temp = [1]
+        for j in range(len(k[i - 1]) - 1):
+            curr = k[i - 1]
+            temp.append(k[i - 1][j] + k[i - 1][j + 1])
+        temp.append(1)
+        k.append(temp)
+    return k
